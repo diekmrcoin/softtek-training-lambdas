7bcec1caec5a2baed831f98f410e931dfbaf14c2
Diego Maroto <diego.diemg@gmail.com>
2023-02-15 19:17:07 +0100

    Changing the lambda data structure for the lambdas communicating themselfs

--- lambdas/lambda-api/main.js
+++ lambdas/lambda-api/main.js
@@ -33,6 +33,9 @@ module.exports.handler = async (event, context) => {
   const response = JSON.parse(result);
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(response),
   };
 };
