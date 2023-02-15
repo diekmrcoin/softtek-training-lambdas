21f9045f85d33d960cf3a8e646cbc7fa33a61e31
Diego Maroto <diego.diemg@gmail.com>
2023-02-15 17:13:06 +0100

    Added lambda and s3 terraform modules

---
+++ terraform/modules/lambda/lambda.tf
@@ -0,0 +1,165 @@
+variable "prefix" { type = string }
+variable "name" { type = string }
+variable "description" { type = string }
+
+variable "edge" {
+  type    = bool
+  default = false
+}
+
+variable "environment" { type = string }
+
+variable "event_sources" {
+  type = list(object({
+    arn        = string
+    batch_size = number
+  }))
+  default = []
+}
+
+variable "handler" { type = string }
+
+variable "memory_size" {
+  type    = string
+  default = "128"
+}
+
+
+variable "permissions" {
+  type    = set(string)
+  default = []
+}
+
+variable "runtime" {
+  type    = string
+  default = "nodejs16.x"
+}
+
+variable "timeout" {
+  type    = string
+  default = "30"
+}
+
+variable "variables" {
+  type    = map(string)
+  default = null
+}
+
+variable "vpc" {
+  type = object({
+    id                     = string
+    cidr_block             = string
+    subnet_private_a       = object({ id = string })
+    subnet_private_b       = object({ id = string })
+    subnet_private_c       = object({ id = string })
+    security_group_private = object({ id = string })
+  })
+  default = null
+}
+
+output "arn" { value = aws_lambda_function.lambda_function.arn }
+output "invoke_arn" { value = aws_lambda_function.lambda_function.invoke_arn }
+output "name" { value = aws_lambda_function.lambda_function.function_name }
+output "qualified_arn" { value = aws_lambda_function.lambda_function.qualified_arn }
+
+locals {
+  permissions_lookup = {
+    dynamodb = "AmazonDynamoDBFullAccess"
+    lambda   = "AWSLambda_FullAccess"
+    s3       = "AmazonS3FullAccess"
+    sns      = "AmazonSNSFullAccess"
+    ssm      = "AmazonSSMReadOnlyAccess"
+  }
+}
+
+resource "aws_iam_role" "iam_role" {
+  name = "${var.prefix}-${var.environment}-iam-lmb-${var.name}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = var.edge ? ["lambda.amazonaws.com", "edgelambda.amazonaws.com"] : ["lambda.amazonaws.com"]
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_AWSLambdaBasicExecutionRole" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment" {
+  for_each   = var.permissions
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/${lookup(local.permissions_lookup, each.value, "ERROR")}"
+}
+
+data "archive_file" "dummy_lambda_archive" {
+  type        = "zip"
+  output_path = "${path.module}/lambda.zip"
+
+  source {
+    content  = "dummy"
+    filename = "dummy.txt"
+  }
+}
+
+provider "aws" {
+  alias  = "lambda"
+  region = var.edge ? "us-east-1" : "eu-west-2"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  provider      = aws.lambda
+  architectures = [var.edge ? "x86_64" : "arm64"]
+  description   = var.description
+  filename      = data.archive_file.dummy_lambda_archive.output_path
+  function_name = "oaap-${var.environment}-lmb-${var.name}"
+  handler       = var.handler
+  memory_size   = var.memory_size
+  package_type  = "Zip"
+  publish       = var.edge
+  role          = aws_iam_role.iam_role.arn
+  runtime       = var.runtime
+  timeout       = var.timeout
+
+  dynamic "environment" {
+    for_each = var.variables != null ? [var.variables] : []
+    content {
+      variables = var.variables
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = var.vpc != null ? [var.vpc] : []
+    content {
+      security_group_ids = [var.vpc.security_group_private.id]
+      subnet_ids         = [var.vpc.subnet_private_a.id, var.vpc.subnet_private_b.id, var.vpc.subnet_private_c.id]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_AWSLambdaDynamoDBExecutionRole" {
+  count      = length(var.event_sources) > 0 ? 1 : 0
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_AWSLambdaVPCAccessExecutionRole" {
+  count      = var.vpc == null ? 0 : 1
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_lambda_event_source_mapping" "lambda_event_source_mapping" {
+  count             = length(var.event_sources)
+  batch_size        = var.event_sources[count.index].batch_size
+  event_source_arn  = var.event_sources[count.index].arn
+  function_name     = aws_lambda_function.lambda_function.arn
+  starting_position = "LATEST"
+}
---
+++ terraform/modules/s3/s3.tf
@@ -0,0 +1,70 @@
+variable "prefix" { type = string }
+variable "environment" { type = string }
+variable "name" { type = string }
+variable "encrypted" {
+  type    = bool
+  default = true
+}
+variable "origin_access_identity" {
+  type = object({
+    iam_arn = string
+  })
+  default = null
+}
+
+output "id" { value = aws_s3_bucket.s3_bucket.id }
+output "arn" { value = aws_s3_bucket.s3_bucket.arn }
+output "bucket_regional_domain_name" { value = aws_s3_bucket.s3_bucket.bucket_regional_domain_name }
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "${var.prefix}-${var.environment}-s3-${var.name}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  count  = var.origin_access_identity != null ? 1 : 0
+  bucket = aws_s3_bucket.s3_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = var.origin_access_identity.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.s3_bucket.arn}/*"
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = var.origin_access_identity.iam_arn
+        },
+        Action   = "s3:ListBucket",
+        Resource = aws_s3_bucket.s3_bucket.arn
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_server_side_encryption_configuration" {
+  count  = var.encrypted ? 1 : 0
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
